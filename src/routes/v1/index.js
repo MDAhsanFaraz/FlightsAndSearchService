351621223029eac5e318dc52e3d47534cb599285
Faraz <faraz.com>
2023-08-12 00:33:25 +0530

    Added more routers for city api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,4 +5,6 @@ const router =express.Router();
 
 router.post('/city',Citycontroller.create);
 router.delete('/city/:id',Citycontroller.destroy);
+router.get('/city/:id',Citycontroller.get);
+router.patch('/city/:id',Citycontroller.update);
 module.exports=router;
