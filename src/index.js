322a556472ea6665e830323ef8e12ba70cbf82aa
Faraz <faraz.com>
2023-08-14 23:26:56 +0530

    Added airplanes

--- src/index.js
+++ src/index.js
@@ -2,27 +2,27 @@ const express = require("express");
 const bodyparser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig');
-const ApiRoutes =require('./routes/index');
+const ApiRoutes = require('./routes/index');
 
-const db=require('./models/index');
-// const {City,Airports}=require('./models/index');
+const db = require('./models/index');
+// const { Airplane } = require('./models/index');
 
- 
+
 const setupAndStartServer = async () => {
-          // create the express object
-          const app = express();
+    // create the express object
+    const app = express();
 
-          app.use(bodyparser.json());
-          app.use(bodyparser.urlencoded({ extended: true }));
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: true }));
 
-          app.use('/api',ApiRoutes);
- 
-          app.listen(PORT, async () => {
-                    console.log(`Server started at ${PORT}`);
-                    if(process.env.SYNC_DB){
-                              db.sequelize.sync({alter:true});
-                    }
-          });
+    app.use('/api', ApiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`Server started at ${PORT}`);
+        if (process.env.SYNC_DB) {
+            db.sequelize.sync({ alter: true });
+        }
+    });
 }
 
 setupAndStartServer(); 
