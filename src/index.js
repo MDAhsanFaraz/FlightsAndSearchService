0d7c7b1f6c3a36d46ab7d20329a8e76a0548f18e
Faraz <faraz.com>
2024-03-04 18:19:30 +0530

    added commment for db sync

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 
-const { PORT } = require('./config/serverConfig');
-const ApiRoutes = require('./routes/index');
+const { PORT } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index");
 
-const db = require('./models/index');
-const { Airplane } = require('./models/index');
-
+const db = require("./models/index");
+const { Airplane } = require("./models/index");
 
 const setupAndStartServer = async () => {
-    // create the express object
-    const app = express();
+  // create the express object
+  const app = express();
 
-    app.use(bodyparser.json());
-    app.use(bodyparser.urlencoded({ extended: true }));
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({ extended: true }));
 
-    app.use('/api', ApiRoutes);
+  app.use("/api", ApiRoutes);
 
-    app.listen(PORT, async () => {
-        console.log(`Server started at ${PORT}`);
+  app.listen(PORT, async () => {
+    console.log(`Server started at ${PORT}`);
+    // db sync is same as migration we do it manually it will create modle in the data base and the tables just like migrations
     //     if (process.env.SYNC_DB) {
     //         db.sequelize.sync({ alter: true });
     //     }
-    });
-}
+  });
+};
 
-setupAndStartServer(); 
+setupAndStartServer();
